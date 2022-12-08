dc9d9172453ab183791ed0f886d0b27a153db25f
notlega <onghinjourn@gmail.com>
2022-12-09 00:44:36 +0800

    feat: :sparkles: integrates supabase

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,29 @@
-import '../styles/globals.css';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import Layout from '../components/Layout';
-import 'material-symbols/outlined.css';
+import '../styles/globals.css';
+import 'react-loading-skeleton/dist/skeleton.css';
 
-const App = ({ Component, pageProps }) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-);
+const App = ({ Component, pageProps }) => {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </QueryClientProvider>
+    </SessionContextProvider>
+  );
+};
 
 App.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
